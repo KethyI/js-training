9d6c7fc9f70604a652ded9fd60576d5f14f53af2
KethyI <kot1256@gmail.com>
2025-12-17 05:20:57 +0200

    if statement, '?'

--- alert.js
+++ alert.js
@@ -1,7 +1,44 @@
-5 > 4 // true
-"ананас" > "яблуко" // false
-"2" > "12" // true
-undefined == null // true
-undefined === null //false
-null == "\n0\n" //false
-null === +"\n0\n" //false
+function a(){
+    let question = prompt("Write any number", 0);
+    if(question > 0){
+        alert("1");
+    }
+    else if (question < 0 ) {
+        alert("-1");
+
+    }
+    else {
+             alert("0");   
+    }
+}
+
+a();
+
+//task2
+let result = (a + b < 4) ?  'Нижче' : 'Вище';
+
+//task3
+let message = (login == 'Працівник') ? 'Привіт':
+ (login == 'Директор') ? 'Вітаю':
+ (login == '') ? 'Немає логіну' :
+  '';
+
+  //task4
+  let value = prompt('Яка "офіційна" назва JavaScript?', '');
+
+    if (value == 'ECMAScript') {
+      alert('Правильно!');
+    } else {
+      alert('Ви не знаєте? ECMAScript!');
+    }
+
+    // task5
+    let value = prompt('Введіть число', 0);
+
+if (value > 0) {
+  alert( 1 );
+} else if (value < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
