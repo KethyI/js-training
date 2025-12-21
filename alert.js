f1b83b5b839644c3f91653bb093bc77880c6e3fc
KethyI <kot1256@gmail.com>
2025-12-21 05:51:39 +0200

    else if/switch

--- alert.js
+++ alert.js
@@ -1,61 +1,32 @@
-// // task1
-// // let i = 0;
-// // while (++i < 5) alert( i );
-// // //1, 2, 3, 4
+// task1
+let browser;
+if (browser == "Edge") {
+    alert( "You've got the Edge!" );}
+else if (browser == "Chrome" 
+  || browser == "Firefox" 
+  || browser == "Safari" 
+  || browser == "Opera") {
+    alert( 'Ми підтримуємо і ці браузери' );}
+else {
+    alert( 'Маємо надію, що ця сторінка виглядає добре!' );
+}
+  
 
-// // let i = 0;
-// // while (i++ < 5) alert( i );
-// // //0, 1, 2, 3, 4, 5
+//task2
 
-// // //task2
-// // for (let i = 0; i < 5; i++) alert( i );
-// // //0, 1, 2, 3, 4
+let a = +prompt('a?', '');
 
-// // for (let i = 0; i < 5; ++i) alert( i );
-// //0, 1, 2, 3, 4
+switch (a){
+  case 0: 
+   alert (0);
+  break;
 
-// //task3
-// for(let i=2; i <= 10; i++) 
-//   {
-//     if (i % 2 == 0) {
-//       alert(i);
-//     }
-//   }
+  case 1: 
+    alert (1);
+    break;
 
-//   //task4
-//   let i = 0;
-//   while (i < 3) {
-//   alert( `число ${i}!` );
-//   i++;
-// }
-
-// //task5
-// let num;
-// do {
-//   num = prompt("Type number over 100", "");
-// } while (num <= 100 && num);
-
-//task6
-let n = 10;
-  for (let i = 2; i<=n; i++) {
-    let isPrime = true;
-    for (let j=2; j<i; j++) {
-      if (i%j==0) {isPrime = false; break;}
-    } 
-    if (isPrime) alert (i);
-  }
-
-
-
-
-//   let n = 10;
-
-// nextPrime:
-// for (let i = 2; i <= n; i++) { // 2, 3, 4, 5, 6, 7, 8, 9, 10
-
-//   for (let j = 2; j < i; j++) { // шукаємо дільник..
-//     if (i % j == 0) continue nextPrime; // не просте, беремо наступне i
-//   }
-
-//   alert( i ); // 2, 3, 5,
-// }
+  case 2:
+  case 3: 
+    alert ("2, 3");
+    break;
+}
