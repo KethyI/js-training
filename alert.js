d9b5b73961efa66b36b86b2c4881b0e32fd900ca
KethyI <kot1256@gmail.com>
2025-12-23 04:49:06 +0200

    arrow functions

--- alert.js
+++ alert.js
@@ -1,21 +1,8 @@
 // task 1.1
-function checkAge(age) { 
-  return (age > 18) ? true : confirm('Батьки дозволили?');
-  }
+let ask = (question, yes, no) =>  confirm(question) ? yes() : no();
 
-//task 1.2
-function checkAge(age) { 
-  return (age > 18) || confirm('Батьки дозволили?');
-  }
-
-//task 2
-function min(a, b) {
-  return (a < b) ? a : b;
-}
-
-//task 3
-function pow() {
-  let x = +prompt("Give natural number for exponentiation")
-  let n = +prompt("Give second number to use as exponent ")
-  return (x%1==0 && n >= 0 && x >= 1 && n%1 == 0) ? (x**n) : alert("use only natural numbers");
-}
+ask(
+  "Ви згодні?",
+  () => alert("Ви погодились.") ,
+  () => alert("Ви скасували виконання.")
+);
