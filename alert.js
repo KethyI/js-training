36d6b9053a6e67e49757acf4b9aeab58b6746480
KethyI <kot1256@gmail.com>
2026-02-11 01:39:12 +0200

    closure

--- alert.js
+++ alert.js
@@ -1,86 +1,50 @@
-// task1.1
-// function sumTo(n) {
-//   let sum = 0;
-//   for (let i = 0; i <= n; i++){
-//     sum += i;
-//   } 
-//   return sum;
-// }
+// task1
 
-// task1.2
-// function sumTo(n) {
-//   let sum = 0;
-//   if (n === 1) return 1;
-//   sum += n + sumTo(n-1);
-//   return sum;
-// }
+function sum(a) {
 
-// task1.3
-function sumTo(n){
-  return n*(n+1)/2;
-  
+  return function (b) {
+    return a+b;
+  }
 }
 
-// task2
-function factorial(n){
-  if (n === 1) return n;
-  return n * factorial(n-1);
+// task 2.1
+function inBetween(a, b) {
+  return function(arg) {
+    return arg >= a && arg <= b;
+  }
+}
+
+// task2.2
+function inArray(arr){
+  return function(item) {
+    return arr.includes(item);
+  }
 }
 
 // task3
-function fib(n) {
-  let result = 0;
-  if (n <= 0) return undefined;
-  if (n === 1) return 1;
-  let a = 0;
-  let b = 1;
-  for (let i = 2; i <= n; i++) {
-    let sum = a + b;
-    a = b;
-    b = sum;
-  }
-  return b;
-}
-
-// task4.1
-// function printList(list){
-//   let container = list;
-//   while (container !== null) {
-//     alert(container.value);
-//     container = container.next;
-    
-//   }
-
-// }
-
-// task4.2
-function printList(list){
-  if (list.value !== null) {
-    alert(list.value);
-    printList(list.next);
+function byField(fieldName) {
+  
+  return function (a, b){
+    return a[fieldName] > b[fieldName] ? 1 : -1;
   }
 }
 
-// task5.1
-function printReverse(list){
-  let arr = [];
+//task4
+function makeArmy() {
+  let shooters = [];
+ 
 
-  while(list) {
-    arr.push(list.value);
-    list = list.next;
+  let i = 0;
+  while (i < 10) {
+    let j = i;
+    let shooter = function() { 
+      
+      alert( j ); 
+    };
+    shooters.push(shooter); 
+    i++;
   }
 
-  let reverseArr = arr.reverse();
-
-  for (let item of reverseArr) {
-    alert(item);
-  }
+  
+  return shooters;
 }
-
-// task5.2
-// function printReverse(list){
-//    if (list === null) return;
-//    printReverse(list.next);
-//    alert(list.value);
-// }
-
